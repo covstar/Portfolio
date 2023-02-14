bb0ab481c6ba3cb852694c8ba3a794e8a25f9c24
Covstar <okechukwuvictor093@gmail.com>
2023-02-14 16:25:07 +0100

    Added image to about session and removed all the other header bg-images

--- js/app.js
+++ js/app.js
@@ -1,17 +1,8 @@
 $(document).ready(function(){
 
-let about_img = 'https://covstar.sirv.com/Portfolio_imgs/D_About.jpg';
-let project_img = 'https://covstar.sirv.com/Portfolio_imgs/D_project.jpg';
-let contact_img = 'https://covstar.sirv.com/Portfolio_imgs/D_contact.jpg';
-
 
     // function for about page 
     $('.About').click(function(){
-        $('header').css({
-            'background': "url("+about_img+")",
-            'background-size': 'cover',
-            'background-repeat': 'no-repeat'
-        });
         $('.Project, .Resume, .Contact').removeClass('active');
         $('.About_section').fadeIn(300);
         $(this).addClass('active');
@@ -19,11 +10,6 @@ let contact_img = 'https://covstar.sirv.com/Portfolio_imgs/D_contact.jpg';
 
     // function for project page 
     $('.Project').click(function(){
-        $('header').css({
-            'background': "url("+project_img+")",
-            'background-size': 'cover',
-            'background-repeat': 'no-repeat'
-        });
         $('.About_section').fadeOut(300);
         $(this).addClass('active');
         $('.About, .Contact, .Resume').removeClass('active');
@@ -38,11 +24,6 @@ let contact_img = 'https://covstar.sirv.com/Portfolio_imgs/D_contact.jpg';
 
     // function for contact page 
     $('.Contact').click(function(){
-        $('header').css({
-            'background': "url("+contact_img+")",
-            'background-size': 'cover',
-            'background-repeat': 'no-repeat'
-        });
         $('.About, .Project, .Resume').removeClass('active');
         $(this).addClass('active');
         $('.About_section').fadeOut(300);
