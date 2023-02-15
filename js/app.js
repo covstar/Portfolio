6ce10d4a764f0f488730b114fb0a32348016da6a
Covstar <okechukwuvictor093@gmail.com>
2023-02-15 11:02:29 +0100

    Added contents to the project page

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ $(document).ready(function(){
     $('.About').click(function(){
         $('.Project, .Resume, .Contact').removeClass('active');
         $('.About_section').fadeIn(300);
+        $('.project_section').fadeOut(300);
         $(this).addClass('active');
     });
 
     // function for project page 
     $('.Project').click(function(){
+        $('.project_section').fadeIn(300);
         $('.About_section').fadeOut(300);
         $(this).addClass('active');
         $('.About, .Contact, .Resume').removeClass('active');
@@ -19,14 +21,14 @@ $(document).ready(function(){
     $('.Resume').click(function(){
         $('.About, .Project, .Contact').removeClass('active');
         $(this).addClass('active');
-        $('.About_section').fadeOut(300);
+        $('.About_section, .project_section').fadeOut(300);
     });
 
     // function for contact page 
     $('.Contact').click(function(){
         $('.About, .Project, .Resume').removeClass('active');
         $(this).addClass('active');
-        $('.About_section').fadeOut(300);
+        $('.About_section, .project_section').fadeOut(300);
     });
 
 });
