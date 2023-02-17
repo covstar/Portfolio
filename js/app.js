ce2ec40fd6ef8c3f1342efe47e07c7ad7c27f2ce
Covstar <okechukwuvictor093@gmail.com>
2023-02-18 00:46:40 +0100

    Added the resume section and also added wahatsapp and scrollToTop button

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
     $('.About').click(function(){
         $('.Project, .Resume, .Contact').removeClass('active');
         $('.About_section').fadeIn(300);
-        $('.project_section').fadeOut(300);
+        $('.project_section, .resume_section, .contact_section').fadeOut(300);
         $(this).addClass('active');
     });
 
     // function for project page 
     $('.Project').click(function(){
         $('.project_section').fadeIn(300);
-        $('.About_section').fadeOut(300);
+        $('.About_section, .resume_section, .contact_section').fadeOut(300);
         $(this).addClass('active');
         $('.About, .Contact, .Resume').removeClass('active');
     });
@@ -21,14 +21,35 @@ $(document).ready(function(){
     $('.Resume').click(function(){
         $('.About, .Project, .Contact').removeClass('active');
         $(this).addClass('active');
-        $('.About_section, .project_section').fadeOut(300);
+        $('.resume_section').fadeIn(300)
+        $('.About_section, .project_section, .contact_section').fadeOut(300);
     });
 
     // function for contact page 
     $('.Contact').click(function(){
         $('.About, .Project, .Resume').removeClass('active');
         $(this).addClass('active');
-        $('.About_section, .project_section').fadeOut(300);
+        $('.contact_section').fadeIn(300);
+        $('.About_section, .project_section, .resume_section').fadeOut(300);
     });
 
 });
+
+// scroll to top function
+$ ('.scrollTop').click (function () {
+  $ ('html, body').animate (
+    {
+      scrollTop: 0,
+    },
+    'slow'
+  );
+  return false;
+});
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    $ ('.scrollTop').css ('display', 'block');
+  } else {
+    $ ('.scrollTop').css ('display', 'none');
+  }
+};
