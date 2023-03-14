4ca5a2d2a6a3b289c751e301a86202167a378ad3
covstar <okechukwuvictor093@gmail.com>
2023-03-14 16:46:01 +0100

    Updated the mood changer

--- js/mood.js
+++ js/mood.js
@@ -1,71 +1,110 @@
 $(document).ready(function() {
 
-    // text colors and background colours toggle function
-$ ('.toggle').click (function () {
-  $ (this).toggleClass ('fa-cloud-sun fa-moon');
+    // text colors and background colours toggle function 
+    $('.toggle').click(function() {
+        $(this).toggleClass('fa-cloud-sun fa-moon');
 
-  if ($ (this).hasClass ('fa-cloud-sun')) {
-    $ (this).addClass ('text-yellow-400');
-    // body overlay
-    $ ('.body_overlay').removeClass ('bg-darker-teal');
-    $ ('.body_overlay').addClass ('bg-lighter-teal');
-    // header_overlay
-    $ ('header').removeClass ('border-b border-b-grey');
-    $ ('.header_overlay').addClass ('bg-black bg-opacity-70');
-    $ ('.header_overlay').removeClass ('bg-darker-teal bg-opacity-90');
+        // for resume section 
+        let resumeBg = '.resume_section > div > a';
+        // for project 
+        let projectBg = '.project_section > div';
+        let projectLink = '.project_section > div > div> ul > li';
 
-    // nav buttons
-    $ ('.nav_buttons > ul > li > button').addClass ('bg-maroon');
-    $ ('.nav_buttons > ul > li > button').removeClass ('bg-darker-teal');
-    $ ('.nav_buttons').removeClass ('text-black');
-    $ ('.nav_buttons').addClass ('text-grey');
 
-    // About section
-    $ ('.About_section > div > img').removeClass ('border-lighter-teal');
-    $ ('.About_section > div > img').addClass ('border-darker-teal');
-    $ ('.About_section > div').addClass ('bg-white text-blackhey');
-    $ ('.About_section > div').removeClass ('bg-darker-teal');
-    $ ('.About_section > div > ul > li').removeClass ('text-grey');
-    $ ('.About_section > div > ul > li').addClass ('text-darker-teal');
-    $ ('.About_section > div > p').removeClass ('text-grey');
-    $ ('.About_section > div > p').addClass ('text-blackhey');
-    $ ('.About_section > div > div').removeClass ('text-grey');
-    $ ('.About_section > div > div').addClass ('text-blackhey');
-    $ ('.About_section > div > div > ul').removeClass ('text-grey');
-    $ ('.About_section > div > div > ul').addClass ('text-blackhey');
-  } else if ($ (this).hasClass ('fa-moon')) {
-    $ (this).removeClass ('text-yellow-400');
-    $ (this).addClass ('text-gray-700');
-    // body overlay
-    $ ('.body_overlay').removeClass ('bg-lighter-teal');
-    $ ('.body_overlay').addClass ('bg-darker-teal');
-    // header_overlay
-    $ ('header').addClass ('border-b border-b-grey');
-    $ ('.header_overlay').addClass ('bg-darker-teal bg-opacity-90');
-    $ ('.header_overlay').removeClass ('bg-black');
-    // nav buttons
-    $ ('.nav_buttons > ul > li > button').addClass ('bg-dark-teal');
-    $ ('.nav_buttons > ul > li > button').removeClass ('bg-maroon');
-    $ ('.nav_buttons').removeClass ('text-grey');
-    $ ('.nav_buttons').addClass ('text-black');
+        if ($(this).hasClass('fa-cloud-sun')) {
+            $(this).addClass('text-lighter-teal');
+            // body overlay 
+            $('.body_overlay').removeClass('bg-darker-teal');
+            $('.body_overlay').addClass('bg-lighter-teal');
+            // header_overlay 
+            $('header').removeClass('border-b border-b-grey');
+            $('.header_overlay').addClass('bg-black bg-opacity-80');
+            $('.header_overlay').removeClass('bg-darker-teal bg-opacity-90');
 
-    // About section
-    $ ('.About_section > div > img').removeClass ('border-darker-teal');
-    $ ('.About_section > div > img').addClass ('border-lighter-teal');
-    $ ('.About_section > div').addClass ('bg-darker-teal text-white');
-    $ ('.About_section > div').removeClass ('bg-white');
-    $ ('.About_section > div > ul > li').removeClass ('text-darker-teal');
-    $ ('.About_section > div > ul > li').addClass ('text-grey');
-    $ ('.About_section > div > p').removeClass ('text-blackhey');
-    $ ('.About_section > div > p').addClass ('text-grey');
-    $ ('.About_section > div > div').removeClass ('text-blackhey');
-    $ ('.About_section > div > div').addClass ('text-grey');
-    $ ('.About_section > div > div > ul').removeClass ('text-blackhey');
-    $ ('.About_section > div > div > ul').addClass ('text-grey');
+            // nav buttons 
+            $('.nav_buttons > ul > li > button').addClass('bg-maroon');
+            $('.nav_buttons > ul > li > button').removeClass('bg-darker-teal');
+            $('.nav_buttons').removeClass('text-black');
+            $('.nav_buttons').addClass('text-grey');
 
-    // Project section
-    $ ('.project_section').removeClass ('');
-  }
-});
+            // About section 
+            // remove grey text color 
+            let removeGreyText = '.About_section > div > ul > li, .About_section > div > p, .About_section > div > div, .About_section > div > div > ul';
+            // add blackhey text color 
+            let addBlackhey = '.About_section > div > p, .About_section > div > div, .About_section > div > div > ul';
+            // add darker teal color 
+            let addDarkerTeal = '.About_section > div > ul > li, .contact_header';
 
+            $('.About_section > div > img').removeClass('border-lighter-teal');
+            $('.About_section > div > img').addClass('border-darker-teal');
+            $('.About_section > div').addClass('bg-white text-blackhey');
+            $('.About_section > div').removeClass('bg-darker-teal');
+            $(removeGreyText).removeClass('text-grey');
+            $(addDarkerTeal).addClass('text-darker-teal');
+            $(addBlackhey).addClass('text-blackhey');
+
+            //  project section 
+            $(projectBg).addClass('bg-white text-blackhey');
+            $(projectBg).removeClass('bg-darker-teal text-grey');
+            $(projectLink).addClass('bg-darker-teal');
+            $(projectLink).removeClass('bg-maroon');
+
+             // resume section 
+            $(resumeBg).addClass('bg-darker-teal');
+            $(resumeBg).removeClass('bg-maroon');
+
+
+            // contact section 
+            $('.contact_header').removeClass('text-lighter-teal');
+
+        }
+        else if ($(this).hasClass('fa-moon')) {
+            $(this).removeClass('text-lighter-teal');
+            $(this).addClass('text-gray-700');
+            // body overlay 
+            $('.body_overlay').removeClass('bg-lighter-teal');
+            $('.body_overlay').addClass('bg-darker-teal');
+            // header_overlay
+            $('header').addClass('border-b border-b-grey');
+            $('.header_overlay').addClass('bg-darker-teal bg-opacity-90');
+            $('.header_overlay').removeClass('bg-black'); 
+            // nav buttons 
+            $('.nav_buttons > ul > li > button').addClass('bg-dark-teal');
+            $('.nav_buttons > ul > li > button').removeClass('bg-maroon');
+            $('.nav_buttons').removeClass('text-grey');
+            $('.nav_buttons').addClass('text-black');
+
+            // About section 
+            // remove grey text color 
+            let addGreyText = '.About_section > div > ul > li, .About_section > div > p, .About_section > div > div, .About_section > div > div > ul';
+             // add blackhey text color 
+            let removeBlackhey = '.About_section > div > p, .About_section > div > div, .About_section > div > div > ul';
+            // add darker teal color 
+            let removeDarkerTeal = '.About_section > div > ul > li, .contact_header';
+
+            $('.About_section > div > img').removeClass('border-darker-teal');
+            $('.About_section > div > img').addClass('border-lighter-teal');
+            $('.About_section > div').addClass('bg-darker-teal text-white');
+            $('.About_section > div').removeClass('bg-white');
+            $(removeDarkerTeal).removeClass('text-darker-teal');
+            $(addGreyText).addClass('text-grey');
+             $(removeBlackhey).removeClass('text-blackhey');
+    
+            //  project section 
+            $(projectBg).removeClass('bg-white text-blackhey');
+            $(projectBg).addClass('bg-darker-teal text-grey');
+            $(projectLink).removeClass('bg-darker-teal');
+            $(projectLink).addClass('bg-maroon');
+
+
+            // contact section 
+            $('.contact_header').addClass('text-lighter-teal');
+
+            // resume section 
+            $(resumeBg).removeClass('bg-darker-teal');
+            $(resumeBg).addClass('bg-maroon');
+            
+            
+        }
+      });
 });
